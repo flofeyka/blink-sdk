201b3641da7de4f4d09a618179362490f470b173
maksfourlife <maksfourlife@gmail.com>
2025-04-21 18:39:11 +0200

    add `initSession` and `obtainSessionKeyPair` functions

---
+++ src/authentication.test.ts
@@ -0,0 +1,32 @@
+import elliptic from "elliptic";
+import { describe, expect, it } from "vitest";
+import { initSession, obtainSessionKeyPair } from "./authentication";
+
+describe("initSession", () => {
+  it("pass", () => {
+    console.log(initSession());
+  });
+});
+
+describe("obtainSessionKey", () => {
+  it("pass", async () => {
+    const privateKey = "f3ad29ed43606839819fe25c2c112880052d574fc2ca85816f34ba13a1a0ca03";
+    const searchParams =
+      "publicKey=BLaX_RN4bvWwHGnKxN6BNV6Y0ppTCjAdU0Tz5VhFkgJRE5x8X8U6ZJPyJeqB07A7TPUO561xK3-4_oHu00HlDZI=&sessionKey=5AvJxdrlyUuBPBBMDQJfUpHjyXHd1lB2RmV0sLj6v6i5GTTd8bIZJpBk_ndJ3e_I&iv=OkhZEAO6Br5uJ5Um";
+
+    const ec = new elliptic.ec("p256");
+
+    const expectedSessionKeyPair = ec.keyFromPrivate([
+      218, 60, 37, 39, 222, 68, 193, 193, 153, 138, 255, 49, 82, 76, 182, 64, 64, 206, 48, 210, 132,
+      179, 233, 213, 137, 143, 199, 1, 204, 2, 230, 43,
+    ]);
+
+    const sessionKeyPair = await obtainSessionKeyPair(
+      privateKey,
+      new URLSearchParams(searchParams)
+    );
+    expect(sessionKeyPair.getPrivate("hex")).eq(expectedSessionKeyPair.getPrivate("hex"));
+
+    // console.log(sessionKeyPair);
+  });
+});
---
+++ src/authentication.ts
@@ -0,0 +1,48 @@
+import elliptic, { type ec } from "elliptic";
+import { z } from "zod";
+
+export async function initSession(): Promise<{ privateKey: string; url: string }> {
+  const ec = new elliptic.ec("p256");
+  const keyPair = ec.genKeyPair();
+  const privateKey = keyPair.getPrivate("hex");
+  const publicKey = Buffer.from(keyPair.getPublic(true, "array")).toString("base64url");
+  return {
+    privateKey,
+    url: `https://t.me/magick_dev277_development3_bot?start=${publicKey}`,
+  };
+}
+
+export async function obtainSessionKeyPair(
+  privateKey: string,
+  searchParams: string | URLSearchParams | Record<string, string>
+): Promise<ec.KeyPair> {
+  const ec = new elliptic.ec("p256");
+
+  const base64url = z.string().transform((t) => Buffer.from(t, "base64url"));
+
+  const searchParamsSchema = z.object({
+    publicKey: base64url.transform((t) => ec.keyFromPublic(t)),
+    sessionKey: base64url,
+    iv: base64url,
+  });
+
+  const { publicKey, sessionKey, iv } = searchParamsSchema.parse(
+    typeof searchParams === "string" || searchParams instanceof URLSearchParams
+      ? Object.fromEntries(new URLSearchParams(searchParams).entries())
+      : searchParams
+  );
+
+  const keyPair = ec.keyFromPrivate(privateKey, "hex");
+
+  const sharedSecret = keyPair.derive(publicKey.getPublic());
+
+  const rawAesKey = await crypto.subtle.digest("SHA-256", sharedSecret.toBuffer());
+
+  const aesKey = await crypto.subtle.importKey("raw", rawAesKey, { name: "AES-GCM" }, false, [
+    "decrypt",
+  ]);
+
+  const decrypted = await crypto.subtle.decrypt({ name: "AES-GCM", iv }, aesKey, sessionKey);
+
+  return ec.keyFromPrivate(Buffer.from(decrypted));
+}
